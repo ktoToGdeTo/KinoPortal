639fe9df1afbb2d7d953d430dca9515ca6c51621
ktoToGdeTo <micha032005@gmail.com>
2025-08-16 20:48:48 +0400

    Добавление авторизации. Реализовано отображение любимых фильмов и актеров для каждого пользователя. TODO: добавить кнопку на страницах актера/кино для добавления/удаления из избранного.

--- src/main/java/com/practice/movieapp/model/Favoriteactor.java
+++ src/main/java/com/practice/movieapp/model/Favoriteactor.java
@@ -1,29 +1,7 @@
 package com.practice.movieapp.model;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import jakarta.persistence.Entity;
 
-@Getter
-@Setter
 @Entity
-@Table(name = "favoriteactor")
 public class Favoriteactor {
-    @EmbeddedId
-    private FavoriteactorId id;
-
-    @MapsId("userId")
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "user_id", nullable = false)
-    private Userportal user;
-
-    @MapsId("actorId")
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "actor_id", nullable = false)
-    private Actor actor;
-
 }
